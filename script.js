f9d7c9d27962cfad57e46f0c6d03f584c16341fd
Grimhamr <rossiadisj@gmail.com>
2021-09-17 01:43:52 +0300

    change on hover, remove scroll

---
+++ script.js
@@ -0,0 +1,53 @@
+let buttons = document.getElementsByClassName("grid-buttons")
+
+let i = 1
+
+while (i <= buttons.length)  {
+    console.log(`button-${i}`)
+    
+    let button = document.getElementById(`button-${i}`)
+    button.addEventListener('mouseenter', e => {
+        button.style.backgroundImage = 'linear-gradient(to right, rgb(0, 140, 150), rgb(0, 255, 155))';
+        console.log("over")
+        
+      });
+      
+      button.addEventListener('mouseleave', e => {
+        button.style.animationDuration = "3s";
+
+        button.style.backgroundImage = 'linear-gradient(to right, rgb(0, 140, 255), rgb(0, 255, 255))';
+
+      });
+      i++;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
