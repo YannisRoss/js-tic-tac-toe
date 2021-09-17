130e15724a1efbd1debd44283db2b0dce168d299
Grimhamr <rossiadisj@gmail.com>
2021-09-17 19:06:33 +0300

    start objects

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 let buttons = document.getElementsByClassName("grid-buttons")
 
 let i = 1
-
-while (i <= buttons.length)  {
-    console.log(`button-${i}`)
-    
-    let button = document.getElementById(`button-${i}`)
-    button.addEventListener('mouseenter', e => {
-        button.style.backgroundImage = 'linear-gradient(to right, rgb(0, 140, 150), rgb(0, 255, 155))';
-        console.log("over")
+    while (i <= buttons.length)  {
+        console.log(`button-${i}`)
         
-      });
-      
-      button.addEventListener('mouseleave', e => {
-        button.style.animationDuration = "3s";
+        let button = document.getElementById(`button-${i}`)
+        button.addEventListener('mouseenter', e => {
+            button.style.backgroundImage = 'linear-gradient(to right, rgb(0, 140, 150), rgb(0, 255, 155))';
+            console.log("over")
+            
+        });
+        
+        button.addEventListener('mouseleave', e => {
+            button.style.animationDuration = "3s";
 
-        button.style.backgroundImage = 'linear-gradient(to right, rgb(0, 140, 255), rgb(0, 255, 255))';
+            button.style.backgroundImage = 'linear-gradient(to right, rgb(0, 140, 255), rgb(0, 255, 255))';
 
-      });
-      i++;
+        });
+        i++;
+
+    }
+
+
+function Gameboard() {
+    this.gridArray =    [null,null,null,
+                        null,null,null,
+                        null,null,null];
+    
+}
+
+
+function Player(name, symbol) {
+    this.name = name;
+    this.wins
+    this.symbol = symbol
 
 }
+
+function Game() {
+    
+
+
+}
+    /*  
+
+
+        player object
+            wins
+
+    */
 
 
 
